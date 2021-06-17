1b91b0eee4d3022c492925fb6b06d9428ee0575c
InJae Lee <69920975+oliver0922@users.noreply.github.com>
2021-06-17 14:03:38 +0900

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -37,7 +37,7 @@ class FasterRCNNTrainer(nn.Module):
     Args:
         faster_rcnn (model.FasterRCNN):
             A Faster R-CNN model that is going to be trained.
-    """
+   
 
     def __init__(self, faster_rcnn):
         super(FasterRCNNTrainer, self).__init__()
@@ -128,20 +128,19 @@ class FasterRCNNTrainer(nn.Module):
             gt_rpn_label.data,
             self.rpn_sigma) #rpn localization loss 계산
 
-        # NOTE: default value of ignore_index is -100 ...
         rpn_cls_loss = F.cross_entropy(rpn_score, gt_rpn_label.cuda(), ignore_index=-1) #cross entropy방식 사용하여 rpn classification 계산
         _gt_rpn_label = gt_rpn_label[gt_rpn_label > -1]
         _rpn_score = at.tonumpy(rpn_score)[at.tonumpy(gt_rpn_label) > -1]
         self.rpn_cm.add(at.totensor(_rpn_score, False), _gt_rpn_label.data.long())
 
-        # ------------------ ROI losses (fast rcnn loss) -------------------#
+        # ------------------ ROI losses 계산 -------------------#
         n_sample = roi_cls_loc.shape[0]
         roi_cls_loc = roi_cls_loc.view(n_sample, -1, 4)
         roi_loc = roi_cls_loc[t.arange(0, n_sample).long().cuda(), \
                               at.totensor(gt_roi_label).long()]
         gt_roi_label = at.totensor(gt_roi_label).long()
         gt_roi_loc = at.totensor(gt_roi_loc)
-
+        # ground-truth와 비교 위해 gt_roi_label과 loc 정리.
         roi_loc_loss = _fast_rcnn_loc_loss(
             roi_loc.contiguous(),
             gt_roi_loc,
@@ -149,20 +148,22 @@ class FasterRCNNTrainer(nn.Module):
             self.roi_sigma)
 
         roi_cls_loss = nn.CrossEntropyLoss()(roi_score, gt_roi_label.cuda())
+        # roi에 대한 loss 계산.
+        # crossentropy loss 함수 적용 하여, roi score와 roi ground truth를 넣는다.
 
         self.roi_cm.add(at.totensor(roi_score, False), gt_roi_label.data.long())
 
         losses = [rpn_loc_loss, rpn_cls_loss, roi_loc_loss, roi_cls_loss]
-        losses = losses + [sum(losses)]
+        losses = losses + [sum(losses)] # 모든 loss더해서 최종 loss 구하기.
 
-        return LossTuple(*losses)
+        return LossTuple(*losses) # loss tuple 형식 반환.
 
     def train_step(self, imgs, bboxes, labels, scale):
-        self.optimizer.zero_grad()
-        losses = self.forward(imgs, bboxes, labels, scale)
-        losses.total_loss.backward()
-        self.optimizer.step()
-        self.update_meters(losses)
+        self.optimizer.zero_grad() # gradient zero 로 초기화.
+        losses = self.forward(imgs, bboxes, labels, scale) # foward propogation 진행. / loss 반환
+        losses.total_loss.backward() # loss로 backward propogation 진행.
+        self.optimizer.step() # gradient로 optimization 진행
+        self.update_meters(losses)  # weight, bias 업데이트.
         return losses
 
     def save(self, save_optimizer=False, save_path=None, **kwargs):
@@ -177,15 +178,16 @@ class FasterRCNNTrainer(nn.Module):
         Returns:
             save_path(str): the path to save models.
         """
-        save_dict = dict()
+        save_dict = dict() # 여러 정보담을 dictionary 생성.
 
-        save_dict['model'] = self.faster_rcnn.state_dict()
-        save_dict['config'] = opt._state_dict()
-        save_dict['other_info'] = kwargs
-        save_dict['vis_info'] = self.vis.state_dict()
+        save_dict['model'] = self.faster_rcnn.state_dict() # model의 architecture 저장.
+        save_dict['config'] = opt._state_dict() # config file, 즉 hyperparameter 저장.
+        save_dict['other_info'] = kwargs 
+        save_dict['vis_info'] = self.vis.state_dict() # 상태에 관한 정보들이 담긴 dictionary를 visdom으로 올리는 작업 저장.
 
         if save_optimizer:
             save_dict['optimizer'] = self.optimizer.state_dict()
+            # optimization 저장.
 
         if save_path is None:
             timestr = time.strftime('%m%d%H%M')
@@ -196,38 +198,47 @@ class FasterRCNNTrainer(nn.Module):
         save_dir = os.path.dirname(save_path)
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
+            # checkpoint를 저장할 경로를 만들고 그 디렉토리에 저장.
+            # 디렉토리가 없으면 만들고 있으면 만들지 않음.
 
         t.save(save_dict, save_path)
         self.vis.save([self.vis.env])
+        # visdom 정보도 저장.
         return save_path
 
     def load(self, path, load_optimizer=True, parse_opt=False, ):
         state_dict = t.load(path)
         if 'model' in state_dict:
             self.faster_rcnn.load_state_dict(state_dict['model'])
-        else:  # legacy way, for backward compatibility
+            # 모델 architecture 불러오기.
+        else: 
             self.faster_rcnn.load_state_dict(state_dict)
             return self
         if parse_opt:
             opt._parse(state_dict['config'])
+            # hyperparameter 가져오기.
         if 'optimizer' in state_dict and load_optimizer:
             self.optimizer.load_state_dict(state_dict['optimizer'])
+            # optimizer 정보 가져오기.
         return self
 
     def update_meters(self, losses):
         loss_d = {k: at.scalar(v) for k, v in losses._asdict().items()}
         for key, meter in self.meters.items():
             meter.add(loss_d[key])
+            # parameter 업데이트를 위한 함수.
+            # loss를 통해 계산한 gradient로 업데이트.
 
     def reset_meters(self):
         for key, meter in self.meters.items():
             meter.reset()
         self.roi_cm.reset()
         self.rpn_cm.reset()
-
+        # roi, rpn의 confusion matrix 초기화.
+        # confusion matrix를 초기화함으로써 mAP를 구하는 matrix가 초기화되는 것.
     def get_meter_data(self):
         return {k: v.value()[0] for k, v in self.meters.items()}
-
+        
 
 def _smooth_l1_loss(x, t, in_weight, sigma):
     sigma2 = sigma ** 2
@@ -237,15 +248,16 @@ def _smooth_l1_loss(x, t, in_weight, sigma):
     y = (flag * (sigma2 / 2.) * (diff ** 2) +
          (1 - flag) * (abs_diff - 0.5 / sigma2))
     return y.sum()
+    # Legularization 1 Loss 사용.
 
 
 def _fast_rcnn_loc_loss(pred_loc, gt_loc, gt_label, sigma):
     in_weight = t.zeros(gt_loc.shape).cuda()
-    # Localization loss is calculated only for positive rois.
-    # NOTE:  unlike origin implementation, 
-    # we don't need inside_weight and outside_weight, they can calculate by gt_label
+    # Localization의 loss는 positive roi로만 계산됨.
+    
     in_weight[(gt_label > 0).view(-1, 1).expand_as(in_weight).cuda()] = 1
     loc_loss = _smooth_l1_loss(pred_loc, gt_loc, in_weight.detach(), sigma)
-    # Normalize by total number of negtive and positive rois.
-    loc_loss /= ((gt_label >= 0).sum().float()) # ignore gt_label==-1 for rpn_loss
+    # 예측값 localization과 ground-truth localization을 L1 loss적용해서 loss 계산. / sigma parameter 적용.
+    
+    loc_loss /= ((gt_label >= 0).sum().float())
     return loc_loss
